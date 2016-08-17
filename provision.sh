612672150fabfa08d990db1df6473cbc9c2a77a5
Vyacheslav Voronenko <git@voronenko.info>
2016-08-17 18:51:02 +0300

    initial commit

---
+++ provision.sh
@@ -0,0 +1,26 @@
+# Static parameters
+WORKSPACE=$PWD
+BOX_PLAYBOOK=$WORKSPACE/mongo_setup.yml
+BOX_NAME=lab21
+BOX_ADDRESS=192.168.0.21  # .15 -- 16.04
+BOX_USER=slavko
+BOX_PWD=
+
+# Register the new Prudentia box, provision it with the staging artifact and eventually unregisters the box
+prudentia ssh <<EOF
+unregister $BOX_NAME
+
+register
+$BOX_PLAYBOOK
+$BOX_NAME
+$BOX_ADDRESS
+$BOX_USER
+$BOX_PWD
+
+verbose 4
+
+provision $BOX_NAME
+
+unregister $BOX_NAME
+EOF
+
